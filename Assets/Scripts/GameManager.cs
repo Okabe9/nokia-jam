b591807357bfc3b30a06a1829bd89d78bfaa8493
Sergi Fernández <sergixghost@gmail.com>
2024-02-21 00:19:09 +0100

    Selecting sections

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,16 +6,87 @@ public class GameManager : MonoBehaviour
 {
   public static GameManager instance;
 
-  // Create a double list 
+  // Create a 3x3 grid of Vector2 accesible from editor
+  [SerializeField] Vector2[] borderPositionsArray;
+  [SerializeField] Vector2[,] borderPositionsGrid;
+
+  [SerializeField] GameObject hoverBorderPrefab;
+  [SerializeField] GameObject activeBorderPrefab;
+
+  private GameObject hoverBorderInstance;
+  private List<GameObject> activatedBorderInstances = new List<GameObject>();
+
+
+  // Singleton pattern
+  private void Awake()
+  {
+    if (instance == null)
+    {
+      instance = this;
+    }
+    else if (instance != this)
+    {
+      Destroy(gameObject);
+    }
+  }
+
+
   // Start is called before the first frame update
   void Start()
   {
-
+    Vector2[,] borderPositionsGrid = GetGrid();
+    // Instantiate the hoverBorderPrefab at the 0, 0position
+    hoverBorderInstance = Instantiate(hoverBorderPrefab, borderPositionsGrid[0, 0], Quaternion.identity);
   }
 
   // Update is called once per frame
   void Update()
   {
+
+  }
+  public Vector2[,] GetGrid()
+  {
+    Vector2[,] newArray = new Vector2[3, 3];
+
+    // Populate the 2D array with elements from the 1D array
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 3; j++)
+      {
+        newArray[i, j] = borderPositionsArray[i * 3 + j];
+      }
+    }
+
+    return newArray;
+  }
+  public void UpdateBorderPosition(int gridX, int gridY)
+  {
+    Vector2[,] grid = GetGrid();
+    Vector2 borderPosition = grid[gridY, gridX];
+    hoverBorderInstance.transform.position = borderPosition;
+
+  }
+  public bool FreezeSection(int gridX, int gridY)
+  {
+    Vector2[,] grid = GetGrid();
+    Vector2 borderPosition = grid[gridY, gridX];
+    foreach (GameObject border in activatedBorderInstances)
+    {
+      Vector2 activeBorderPosition = new Vector2(border.transform.position.x, border.transform.position.y);
+      if (borderPosition == activeBorderPosition)
+      {
+        activatedBorderInstances.Remove(border);
+        Destroy(border);
+        print("Unfreezed Section" + gridX + " " + gridY);
+
+        return false;
+      }
+
+    }
+    GameObject activeBorderInstance = Instantiate(activeBorderPrefab, borderPosition, Quaternion.identity);
+    activatedBorderInstances.Add(activeBorderInstance);
+    print("Freezed Section" + gridX + " " + gridY);
+    return true;
 
   }
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+  int gridX = 0;
+  int gridY = 0;
   // Start is called before the first frame update
   void Start()
   {
@@ -15,20 +17,34 @@ public class PlayerInput : MonoBehaviour
   {
     if (Input.GetKeyDown(KeyCode.Space))
     {
-
+      GameManager.instance.FreezeSection(gridX, gridY);
     }
     if (Input.GetKeyDown(KeyCode.W))
     {
+      gridY = (gridY + 2) % 3;
+      UpdateBorderPosition();
     }
     if (Input.GetKeyDown(KeyCode.A))
     {
+      gridX = (gridX + 2) % 3;
+      UpdateBorderPosition();
+
     }
     if (Input.GetKeyDown(KeyCode.S))
     {
+      gridY = (gridY + 1) % 3;
+      UpdateBorderPosition();
+
     }
     if (Input.GetKeyDown(KeyCode.D))
     {
-    }
+      gridX = (gridX + 1) % 3;
+      UpdateBorderPosition();
 
+    }
+  }
+  void UpdateBorderPosition()
+  {
+    GameManager.instance.UpdateBorderPosition(gridX, gridY);
   }
 }
