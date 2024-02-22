e998f7b3bca34634434a8c130c0cce2b622e6dd7
Eduard Minguell <60965207+Eduardiko@users.noreply.github.com>
2024-02-22 17:27:55 +0100

    Collision Continuity

--- Assets/Scripts/TimeStopableEntity.cs
+++ Assets/Scripts/TimeStopableEntity.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 public abstract class TimeStoppableEntity : MonoBehaviour
 {
-  public bool isTimeStopped = false;
+    public bool isTimeStopped = false;
+    public bool isTransitioningBehindFreeze = false;
+    public int originalLayer;
 
-  public void StopTime()
-  {
-    isTimeStopped = true;
-  }
-  public void StartTime()
-  {
-    isTimeStopped = false;
-  }
-  public void ToggleTime()
-  {
-    isTimeStopped = !isTimeStopped;
-  }
+    private void Start()
+    {
+        originalLayer = gameObject.layer;
+    }
+
+    public void StopTime()
+    {
+        isTimeStopped = true;
+    }
+    public void StartTime()
+    {
+        isTimeStopped = false;
+    }
+    public void ToggleTime()
+    {
+        isTimeStopped = !isTimeStopped;
+    }
 }
