d51848d41c85ca6c9443a538249c135ebb0c9454
Eduard Minguell <60965207+Eduardiko@users.noreply.github.com>
2024-02-23 23:36:42 +0100

    New Level 1 + Freeze Cooldown + Pause Anim On Freeze

--- Assets/Scripts/LemmingController.cs
+++ Assets/Scripts/LemmingController.cs
@@ -4,133 +4,148 @@ using UnityEngine;
 
 public class LemmingController : TimeStoppableEntity
 {
-  float timeRemaining = 0;
-  int currentDirection = 1;
+    float timeRemaining = 0;
+    int currentDirection = 1;
 
-  [SerializeField]
-  float walkTime = 0.2f;
-  bool imDead = false;
+    [SerializeField]
+    float walkTime = 0.2f;
+    bool imDead = false;
 
 
-  [SerializeField]
-  GameObject lemming;
+    [SerializeField]
+    GameObject lemming;
 
-  private Vector3 previousVelocity;
-  private float previousAngularVelocity;
+    private Vector3 previousVelocity;
+    private float previousAngularVelocity;
 
-  private bool isManuallyFrozen = false;
+    private bool isManuallyFrozen = false;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    timeRemaining = walkTime;
-  }
+    [SerializeField] private float freezeCooldownTime = 2f;
+    private float freezeCooldownTimer = 0f;
 
-  // Update is called once per frame
-  void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.F))
+    private Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
     {
-      Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        timeRemaining = walkTime;
 
-      if (!isManuallyFrozen)
-      {
-        isManuallyFrozen = true;
-
-        previousVelocity = rb.velocity;
-        rb.velocity = Vector2.zero;
-
-        rb.isKinematic = true;
-      }
-      else
-      {
-        isManuallyFrozen = false;
-        isTimeStopped = false;
-
-        rb.bodyType = RigidbodyType2D.Dynamic;
-        rb.velocity = previousVelocity;
-      }
+        animator = gameObject.GetComponent<Animator>();
     }
 
-    print("");
-
-    if (isManuallyFrozen)
-      isTimeStopped = true;
-
-
-    if (WallInFront())
+    // Update is called once per frame
+    void Update()
     {
-      //Change direction
-      currentDirection *= -1;
-      transform.localScale = new Vector3(currentDirection, 1, 1);
-    }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
 
-    if (!isTimeStopped)
-      EntityMovement();
+            if (!isManuallyFrozen && freezeCooldownTimer <=0)
+            {
+                isManuallyFrozen = true;
+
+                previousVelocity = rb.velocity;
+                rb.velocity = Vector2.zero;
+
+                rb.isKinematic = true;
+
+                animator.speed = 0;
+            }
+            else
+            {
+                isManuallyFrozen = false;
+                isTimeStopped = false;
+
+                rb.bodyType = RigidbodyType2D.Dynamic;
+                rb.velocity = previousVelocity;
+
+                freezeCooldownTimer = freezeCooldownTime;
+                animator.speed = 1;
+            }
+        }
 
 
-    if (gameObject.layer == LayerMask.NameToLayer("Player"))
-    {
-      Collider2D[] colliders = Physics2D.OverlapBoxAll(gameObject.GetComponent<BoxCollider2D>().bounds.center, gameObject.GetComponent<BoxCollider2D>().bounds.size, 0f, LayerMask.GetMask("ActiveBorder"));
+        if (freezeCooldownTimer > 0)
+            freezeCooldownTimer -= Time.deltaTime;
 
-      if (colliders.Length > 0)
-        gameObject.GetComponent<SpriteRenderer>().color = GameManager.instance.palletes[GameManager.instance.currentPalleteIndex].backgroundColor;
-      else
-        gameObject.GetComponent<SpriteRenderer>().color = GameManager.instance.palletes[GameManager.instance.currentPalleteIndex].foregroundColor;
-    }
+        if (isManuallyFrozen)
+            isTimeStopped = true;
 
-  }
 
-  bool WallInFront()
-  {
-    bool wallInFront = Physics2D.Raycast(transform.position, new Vector2(currentDirection, 0), 4f, LayerMask.GetMask("Wall"));
-    Debug.DrawRay(transform.position, new Vector2(currentDirection * 4, 0), Color.red);
+        if (WallInFront())
+        {
+            //Change direction
+            currentDirection *= -1;
+            transform.localScale = new Vector3(currentDirection, 1, 1);
+        }
 
-    return wallInFront;
-  }
+        if (!isTimeStopped)
+            EntityMovement();
 
-  public void Death()
-  {
-    if (!imDead)
-    {
-      GameManager.instance.RestartLevel();
-      Destroy(gameObject);
-      imDead = true;
+
+        if (gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Collider2D[] colliders = Physics2D.OverlapBoxAll(gameObject.GetComponent<BoxCollider2D>().bounds.center, gameObject.GetComponent<BoxCollider2D>().bounds.size, 0f, LayerMask.GetMask("ActiveBorder"));
+
+            if (colliders.Length > 0)
+                gameObject.GetComponent<SpriteRenderer>().color = GameManager.instance.palletes[GameManager.instance.currentPalleteIndex].backgroundColor;
+            else
+                gameObject.GetComponent<SpriteRenderer>().color = GameManager.instance.palletes[GameManager.instance.currentPalleteIndex].foregroundColor;
+        }
 
     }
 
-  }
-  public void EntityMovement()
-  {
-    if (timeRemaining > 0)
-      timeRemaining -= Time.deltaTime;
-    else
+    bool WallInFront()
     {
-      //Move one unit forward
-      transform.position += new Vector3(currentDirection, 0, 0);
-      timeRemaining = walkTime;
+        bool wallInFront = Physics2D.Raycast(transform.position, new Vector2(currentDirection, 0), 4f, LayerMask.GetMask("Wall"));
+        Debug.DrawRay(transform.position, new Vector2(currentDirection * 4, 0), Color.red);
+
+        return wallInFront;
     }
-  }
 
-  private void OnCollisionStay2D(Collision2D collision)
-  {
-    if (collision.gameObject.CompareTag("Killer"))
-      Death();
-
-    if (collision.gameObject.CompareTag("MovingPlatform") && !collision.gameObject.GetComponent<TimeStoppableEntity>().isTimeStopped)
-      gameObject.layer = collision.gameObject.layer;
-    else
+    public void Death()
     {
-      gameObject.layer = LayerMask.NameToLayer("Player");
-      if (isTimeStopped)
-        isTimeStopped = false;
-    }
-  }
+        if (!imDead)
+        {
+            GameManager.instance.RestartLevel();
+            Destroy(gameObject);
+            imDead = true;
 
-  private void OnCollisionExit2D(Collision2D collision)
-  {
-    if (collision.gameObject.CompareTag("MovingPlatform"))
-      gameObject.layer = LayerMask.NameToLayer("Player");
-  }
+        }
+
+    }
+    public void EntityMovement()
+    {
+        if (timeRemaining > 0)
+            timeRemaining -= Time.deltaTime;
+        else
+        {
+            //Move one unit forward
+            transform.position += new Vector3(currentDirection, 0, 0);
+            timeRemaining = walkTime;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Killer"))
+            Death();
+
+        if (collision.gameObject.CompareTag("MovingPlatform") && !collision.gameObject.GetComponent<TimeStoppableEntity>().isTimeStopped)
+            gameObject.layer = collision.gameObject.layer;
+        else
+        {
+            gameObject.layer = LayerMask.NameToLayer("Player");
+            if (isTimeStopped)
+                isTimeStopped = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+            gameObject.layer = LayerMask.NameToLayer("Player");
+    }
 
 }
