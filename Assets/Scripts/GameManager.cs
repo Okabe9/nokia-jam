4527e141e4d5c167029cd6abf57af6b4ea6206dc
Eduard Minguell <60965207+Eduardiko@users.noreply.github.com>
2024-02-24 01:46:51 +0100

    Nomacuerdo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,141 +4,149 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-  public static GameManager instance;
+    public static GameManager instance;
 
-  // Create a 3x3 grid of Vector2 accesible from editor
-  [SerializeField] Vector2[] borderPositionsArray;
-  [SerializeField] Vector2[,] borderPositionsGrid;
+    // Create a 3x3 grid of Vector2 accesible from editor
+    [SerializeField] Vector2[] borderPositionsArray;
+    [SerializeField] Vector2[,] borderPositionsGrid;
 
-  [SerializeField] GameObject hoverBorderPrefab;
-  [SerializeField] GameObject activeBorderPrefab;
+    [SerializeField] GameObject hoverBorderPrefab;
+    [SerializeField] GameObject activeBorderPrefab;
 
-  [SerializeField] Vector2 lemmingStartingPosition = new Vector2(0, 0);
-  [SerializeField] private GameObject lemming;
+    [SerializeField] Vector2 lemmingStartingPosition = new Vector2(0, 0);
+    [SerializeField] private GameObject lemming;
+
+    [SerializeField] private int frozenSelectionAmmo = 2;
+
+    private GameObject hoverBorderInstance;
+    private List<GameObject> activatedBorderInstances = new List<GameObject>();
+
+    [SerializeField]
+    public List<Pallete> palletes = new List<Pallete>();
+
+    public int currentPalleteIndex = 0;
 
 
-  private GameObject hoverBorderInstance;
-  private List<GameObject> activatedBorderInstances = new List<GameObject>();
-
-  [SerializeField]
-  public List<Pallete> palletes = new List<Pallete>();
-
-  public int currentPalleteIndex = 0;
-  // Singleton pattern
-  private void Awake()
-  {
-    if (instance == null)
+    private void Awake()
     {
-      instance = this;
-    }
-    else if (instance != this)
-    {
-      Destroy(gameObject);
-    }
-  }
-
-
-  // Start is called before the first frame update
-  void Start()
-  {
-    Vector2[,] borderPositionsGrid = GetGrid();
-    // Instantiate the hoverBorderPrefab at the 0, 0position
-    hoverBorderInstance = Instantiate(hoverBorderPrefab, borderPositionsGrid[0, 0], Quaternion.identity);
-    SpritePainting(); // Call the SpritePainting method
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
-  public Vector2[,] GetGrid()
-  {
-    Vector2[,] newArray = new Vector2[3, 3];
-
-    // Populate the 2D array with elements from the 1D array
-    for (int i = 0; i < 3; i++)
-    {
-      for (int j = 0; j < 3; j++)
-      {
-        newArray[i, j] = borderPositionsArray[i * 3 + j];
-      }
-    }
-
-    return newArray;
-  }
-  public void UpdateBorderPosition(int gridX, int gridY)
-  {
-    Vector2[,] grid = GetGrid();
-    Vector2 borderPosition = grid[gridY, gridX];
-    hoverBorderInstance.transform.position = borderPosition;
-
-  }
-  public bool FreezeSection(int gridX, int gridY)
-  {
-    //Get the selection border class from hoverborderinstance
-    SelectionBorder selectionBorder = hoverBorderInstance.GetComponent<SelectionBorder>();
-    Vector2[,] grid = GetGrid();
-    Vector2 borderPosition = grid[gridY, gridX];
-    foreach (GameObject border in activatedBorderInstances)
-    {
-      Vector2 activeBorderPosition = new Vector2(border.transform.position.x, border.transform.position.y);
-      if (borderPosition == activeBorderPosition)
-      {
-        activatedBorderInstances.Remove(border);
-        Destroy(border);
-        print("Unfreezed Section" + gridX + " " + gridY);
-        selectionBorder.UnfreezeTimeStoppableEntities();
-
-        return false;
-      }
-
-    }
-    GameObject activeBorderInstance = Instantiate(activeBorderPrefab, borderPosition, Quaternion.identity);
-    activatedBorderInstances.Add(activeBorderInstance);
-    print("Freezed Section" + gridX + " " + gridY);
-    selectionBorder.FreezeTimeStoppableEntities();
-
-    return true;
-
-  }
-
-  public void SpritePainting()
-  {
-    GameObject[] gameObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
-    Camera mainCamera = Camera.main;
-    if (mainCamera != null)
-    {
-      mainCamera.backgroundColor = palletes[currentPalleteIndex].backgroundColor;
-    }
-    foreach (GameObject go in gameObjects)
-    {
-      SpriteRenderer spriteRenderer = go.GetComponent<SpriteRenderer>();
-
-      if (go.tag == "ColorChange")
-      {
-        Material material = spriteRenderer.material;
-        material.SetColor("_ForegroundColor", palletes[currentPalleteIndex].foregroundColor);
-        material.SetColor("_BackgroundColor", palletes[currentPalleteIndex].backgroundColor);
-
-      }
-      else if (spriteRenderer != null && go.tag != "ColorChange")
-      {
-        if (go.tag != "Background")
+        if (instance == null)
         {
-          go.GetComponent<SpriteRenderer>().color = palletes[currentPalleteIndex].foregroundColor;
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2[,] borderPositionsGrid = GetGrid();
+        // Instantiate the hoverBorderPrefab at the 0, 0position
+        hoverBorderInstance = Instantiate(hoverBorderPrefab, borderPositionsGrid[0, 0], Quaternion.identity);
+        SpritePainting(); // Call the SpritePainting method
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public Vector2[,] GetGrid()
+    {
+        Vector2[,] newArray = new Vector2[3, 3];
+
+        // Populate the 2D array with elements from the 1D array
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                newArray[i, j] = borderPositionsArray[i * 3 + j];
+            }
+        }
+
+        return newArray;
+    }
+    public void UpdateBorderPosition(int gridX, int gridY)
+    {
+        Vector2[,] grid = GetGrid();
+        Vector2 borderPosition = grid[gridY, gridX];
+        hoverBorderInstance.transform.position = borderPosition;
+
+    }
+    public bool FreezeSection(int gridX, int gridY)
+    {
+        //Get the selection border class from hoverborderinstance
+        SelectionBorder selectionBorder = hoverBorderInstance.GetComponent<SelectionBorder>();
+        Vector2[,] grid = GetGrid();
+        Vector2 borderPosition = grid[gridY, gridX];
+        foreach (GameObject border in activatedBorderInstances)
+        {
+            Vector2 activeBorderPosition = new Vector2(border.transform.position.x, border.transform.position.y);
+            if (borderPosition == activeBorderPosition)
+            {
+                activatedBorderInstances.Remove(border);
+                Destroy(border);
+                selectionBorder.UnfreezeTimeStoppableEntities();
+
+                frozenSelectionAmmo++;
+
+                return false;
+            }
 
         }
-        else
-        {
-          go.GetComponent<SpriteRenderer>().color = palletes[currentPalleteIndex].backgroundColor;
-        }
-      }
+
+        if (frozenSelectionAmmo == 0)
+            return false;
+
+        GameObject activeBorderInstance = Instantiate(activeBorderPrefab, borderPosition, Quaternion.identity);
+        activatedBorderInstances.Add(activeBorderInstance);
+        selectionBorder.FreezeTimeStoppableEntities();
+
+        frozenSelectionAmmo--;
+
+        return true;
+
     }
-  }
-  public void RestartLevel()
-  {
-    AudioManager.instance.PlaySFX("StartLevel");
-    Instantiate(lemming, lemmingStartingPosition, Quaternion.identity);
-  }
+
+    public void SpritePainting()
+    {
+        GameObject[] gameObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+        Camera mainCamera = Camera.main;
+        if (mainCamera != null)
+        {
+            mainCamera.backgroundColor = palletes[currentPalleteIndex].backgroundColor;
+        }
+        foreach (GameObject go in gameObjects)
+        {
+            SpriteRenderer spriteRenderer = go.GetComponent<SpriteRenderer>();
+
+            if (go.tag == "ColorChange")
+            {
+                Material material = spriteRenderer.material;
+                material.SetColor("_ForegroundColor", palletes[currentPalleteIndex].foregroundColor);
+                material.SetColor("_BackgroundColor", palletes[currentPalleteIndex].backgroundColor);
+
+            }
+            else if (spriteRenderer != null && go.tag != "ColorChange")
+            {
+                if (go.tag != "Background")
+                {
+                    go.GetComponent<SpriteRenderer>().color = palletes[currentPalleteIndex].foregroundColor;
+
+                }
+                else
+                {
+                    go.GetComponent<SpriteRenderer>().color = palletes[currentPalleteIndex].backgroundColor;
+                }
+            }
+        }
+    }
+    public void RestartLevel()
+    {
+        AudioManager.instance.PlaySFX("StartLevel");
+        Instantiate(lemming, lemmingStartingPosition, Quaternion.identity);
+    }
 }
--- Assets/Scripts/LemmingController.cs
+++ Assets/Scripts/LemmingController.cs
@@ -4,210 +4,212 @@ using UnityEngine;
 
 public class LemmingController : TimeStoppableEntity
 {
-  float timeRemaining = 0;
-  int currentDirection = 1;
+    float timeRemaining = 0;
+    int currentDirection = 1;
 
-  [SerializeField]
-  float walkTime = 0.2f;
-  bool imDead = false;
-
-
-  [SerializeField]
-  GameObject lemming;
-
-  private Vector3 previousVelocity;
-  private float previousAngularVelocity;
-
-  private bool isManuallyFrozen = true;
-
-  [SerializeField] private float freezeCooldownTime = 2f;
-  private float freezeCooldownTimer = 0f;
-
-  private Animator animator;
+    [SerializeField]
+    float walkTime = 0.2f;
+    bool imDead = false;
 
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    timeRemaining = walkTime;
+    [SerializeField]
+    GameObject lemming;
 
-    animator = gameObject.GetComponent<Animator>();
-  }
+    private Vector3 previousVelocity;
+    private float previousAngularVelocity;
 
-  // Update is called once per frame
-  void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.F))
+    private bool isManuallyFrozen = true;
+
+    [SerializeField] private float freezeCooldownTime = 2f;
+    private float freezeCooldownTimer = 0f;
+
+    private Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
     {
-      Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        timeRemaining = walkTime;
 
-      if (!isManuallyFrozen && freezeCooldownTimer <= 0)
-      {
-        isManuallyFrozen = true;
-        AudioManager.instance.PlaySFX("FreezeLemming");
-        previousVelocity = rb.velocity;
-        rb.velocity = Vector2.zero;
-
-        rb.isKinematic = true;
-
-        animator.speed = 0;
-      }
-      else
-      {
-        isManuallyFrozen = false;
-        isTimeStopped = false;
-        AudioManager.instance.PlaySFX("UnfreezeLemming");
-
-        rb.bodyType = RigidbodyType2D.Dynamic;
-        rb.velocity = previousVelocity;
-
-        freezeCooldownTimer = freezeCooldownTime;
-        animator.speed = 1;
-      }
+        animator = gameObject.GetComponent<Animator>();
     }
 
-
-    if (freezeCooldownTimer > 0)
-      freezeCooldownTimer -= Time.deltaTime;
-
-    if (isManuallyFrozen)
-      isTimeStopped = true;
-
-
-    if (WallInFront())
+    // Update is called once per frame
+    void Update()
     {
-      //Change direction
-      currentDirection *= -1;
-      transform.localScale = new Vector3(currentDirection, 1, 1);
-    }
-
-    if (!isTimeStopped)
-      EntityMovement();
-
-
-    if (gameObject.layer == LayerMask.NameToLayer("Player"))
-    {
-      Collider2D[] colliders = Physics2D.OverlapBoxAll(gameObject.GetComponent<BoxCollider2D>().bounds.center, gameObject.GetComponent<BoxCollider2D>().bounds.size, 0f, LayerMask.GetMask("ActiveBorder"));
-
-      if (colliders.Length > 0)
-        gameObject.GetComponent<SpriteRenderer>().color = GameManager.instance.palletes[GameManager.instance.currentPalleteIndex].backgroundColor;
-      else
-        gameObject.GetComponent<SpriteRenderer>().color = GameManager.instance.palletes[GameManager.instance.currentPalleteIndex].foregroundColor;
-    }
-
-  }
-
-  bool WallInFront()
-  {
-    bool wallInFront = Physics2D.Raycast(transform.position, new Vector2(currentDirection, 0), 4f, LayerMask.GetMask("Wall"));
-    Debug.DrawRay(transform.position, new Vector2(currentDirection * 4, 0), Color.red);
-
-    return wallInFront;
-  }
-
-  public void Death()
-  {
-    if (!imDead)
-    {
-      GameManager.instance.RestartLevel();
-      Destroy(gameObject);
-      imDead = true;
-
-    }
-
-  }
-  public void EntityMovement()
-  {
-    if (timeRemaining > 0)
-      timeRemaining -= Time.deltaTime;
-    else
-    {
-      //Move one unit forward
-      transform.position += new Vector3(currentDirection, 0, 0);
-      timeRemaining = walkTime;
-    }
-  }
-
-  private void OnCollisionEnter2D(Collision2D collision)
-  {
-    if (collision.gameObject.CompareTag("MovingPlatform"))
-    {
-      int vertical = 0;
-      int horizontal = 0;
-      float threshold = 0.85f;
-
-      foreach (ContactPoint2D contact in collision.contacts)
-      {
-        Vector2 direction = contact.point - (Vector2)transform.position;
-
-        direction.Normalize();
-
-        if (Vector2.Dot(direction, Vector2.up) > threshold)
+        if (Input.GetKeyDown(KeyCode.F))
         {
-          vertical++;
-        }
-        else if (Vector2.Dot(direction, Vector2.down) > threshold)
-        {
-          vertical--;
-        }
-        else if (Vector2.Dot(direction, Vector2.right) > threshold)
-        {
-          horizontal++;
-        }
-        else if (Vector2.Dot(direction, Vector2.left) > threshold)
-        {
-          horizontal--;
-        }
-      }
+            Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
 
-      if (vertical > 0)
-      {
-        //Up Collision
-      }
-      else if (vertical < 0)
-      {
-        //Down Collision
-      }
-      else if (horizontal > 0)
-      {
-        //Right Collision
-        currentDirection *= -1;
-        transform.localScale = new Vector3(currentDirection, 1, 1);
-        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            if (!isManuallyFrozen && freezeCooldownTimer <= 0)
+            {
+                isManuallyFrozen = true;
+                AudioManager.instance.PlaySFX("FreezeLemming");
+                previousVelocity = rb.velocity;
+                rb.velocity = Vector2.zero;
 
-      }
-      else if (horizontal < 0)
-      {
-        //Left Collision
-        currentDirection *= -1;
-        transform.localScale = new Vector3(currentDirection, 1, 1);
-        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-      }
+                rb.isKinematic = true;
+
+                animator.speed = 0;
+            }
+            else
+            {
+                isManuallyFrozen = false;
+                isTimeStopped = false;
+                AudioManager.instance.PlaySFX("UnfreezeLemming");
+
+                rb.bodyType = RigidbodyType2D.Dynamic;
+                rb.velocity = previousVelocity;
+
+                freezeCooldownTimer = freezeCooldownTime;
+                animator.speed = 1;
+            }
+        }
+
+
+        if (freezeCooldownTimer > 0)
+            freezeCooldownTimer -= Time.deltaTime;
+
+        if (isManuallyFrozen)
+            isTimeStopped = true;
+
+
+        if (WallInFront())
+        {
+            //Change direction
+            currentDirection *= -1;
+            transform.localScale = new Vector3(currentDirection, 1, 1);
+        }
+
+        if (!isTimeStopped)
+            EntityMovement();
+
+
+        if (gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Collider2D[] colliders = Physics2D.OverlapBoxAll(gameObject.GetComponent<BoxCollider2D>().bounds.center, gameObject.GetComponent<BoxCollider2D>().bounds.size, 0f, LayerMask.GetMask("ActiveBorder"));
+
+            if (colliders.Length > 0)
+                gameObject.GetComponent<SpriteRenderer>().color = GameManager.instance.palletes[GameManager.instance.currentPalleteIndex].backgroundColor;
+            else
+                gameObject.GetComponent<SpriteRenderer>().color = GameManager.instance.palletes[GameManager.instance.currentPalleteIndex].foregroundColor;
+        }
+
     }
-  }
 
-
-  private void OnCollisionStay2D(Collision2D collision)
-  {
-    if (collision.gameObject.CompareTag("Killer"))
-      Death();
-
-    if (collision.gameObject.CompareTag("MovingPlatform") && !collision.gameObject.GetComponent<TimeStoppableEntity>().isTimeStopped)
-      gameObject.layer = collision.gameObject.layer;
-    else
+    bool WallInFront()
     {
-      gameObject.layer = LayerMask.NameToLayer("Player");
-      if (isTimeStopped)
-        isTimeStopped = false;
+        bool wallInFront = Physics2D.Raycast(transform.position, new Vector2(currentDirection, 0), 4f, LayerMask.GetMask("Wall"));
+        Debug.DrawRay(transform.position, new Vector2(currentDirection * 4, 0), Color.red);
+
+        return wallInFront;
+    }
+
+    public void Death()
+    {
+        if (!imDead)
+        {
+            GameManager.instance.RestartLevel();
+            Destroy(gameObject);
+            imDead = true;
+
+        }
+
+    }
+    public void EntityMovement()
+    {
+        if (timeRemaining > 0)
+            timeRemaining -= Time.deltaTime;
+        else
+        {
+            //Move one unit forward
+            transform.position += new Vector3(currentDirection, 0, 0);
+            timeRemaining = walkTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        return;
+
+        if (collision.gameObject.CompareTag("MovingPlatform") || collision.gameObject.CompareTag("Ground"))
+        {
+            int vertical = 0;
+            int horizontal = 0;
+            float threshold = 0.85f;
+
+            foreach (ContactPoint2D contact in collision.contacts)
+            {
+                Vector2 direction = contact.point - (Vector2)transform.position;
+
+                direction.Normalize();
+
+                if (Vector2.Dot(direction, Vector2.up) > threshold)
+                {
+                    vertical++;
+                }
+                else if (Vector2.Dot(direction, Vector2.down) > threshold)
+                {
+                    vertical--;
+                }
+                else if (Vector2.Dot(direction, Vector2.right) > threshold)
+                {
+                    horizontal++;
+                }
+                else if (Vector2.Dot(direction, Vector2.left) > threshold)
+                {
+                    horizontal--;
+                }
+            }
+
+            if (vertical > 0)
+            {
+                //Up Collision
+            }
+            else if (vertical < 0)
+            {
+                //Down Collision
+            }
+            else if (horizontal > 0)
+            {
+                //Right Collision
+                currentDirection *= -1;
+                transform.localScale = new Vector3(currentDirection, 1, 1);
+                gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+            }
+            else if (horizontal < 0)
+            {
+                //Left Collision
+                currentDirection *= -1;
+                transform.localScale = new Vector3(currentDirection, 1, 1);
+                gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            }
+        }
     }
 
 
-  }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Killer"))
+            Death();
 
-  private void OnCollisionExit2D(Collision2D collision)
-  {
-    if (collision.gameObject.CompareTag("MovingPlatform"))
-      gameObject.layer = LayerMask.NameToLayer("Player");
-  }
+        if (collision.gameObject.CompareTag("MovingPlatform") && !collision.gameObject.GetComponent<TimeStoppableEntity>().isTimeStopped)
+            gameObject.layer = collision.gameObject.layer;
+        else
+        {
+            gameObject.layer = LayerMask.NameToLayer("Player");
+            if (isTimeStopped)
+                isTimeStopped = false;
+        }
+
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovingPlatform"))
+            gameObject.layer = LayerMask.NameToLayer("Player");
+    }
 
 }
